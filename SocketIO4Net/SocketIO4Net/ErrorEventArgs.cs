ad3b8b426dd814a5d002783b7fb891064d8e357f
SUN Chenliang <louis.scl@gmail.com>
2012-11-21 17:25:28 +0100

    socketio for w8

---
+++ SocketIO4Net/SocketIO4Net/ErrorEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Web;
+
+namespace SocketIOClient
+{
+	public class ErrorEventArgs : EventArgs
+	{
+		
+		//public string Message { get; set; }
+		public Exception Exception { get; set; }
+        public WebErrorStatus ErrorStatus { get; set; }
+
+        public ErrorEventArgs(WebErrorStatus webErrorStatus)
+            : base()
+		{
+            this.ErrorStatus = webErrorStatus;
+		}
+        public ErrorEventArgs(WebErrorStatus webErrorStatus, Exception exception)
+            : base()
+		{
+            this.ErrorStatus = webErrorStatus;
+			this.Exception = exception;
+		}
+
+
+	}
+}
---
+++ SocketIO4Net/SocketIOTest/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json.Linq;
+using SocketIOClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Sockets;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SocketIOTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Client socketIO;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+
+        void socketIO_Message(object sender, MessageEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.Message.Event))
+            {
+                Debug.WriteLine("--> SOCKET_IO_MESSAGE:: {0}", e.Message.MessageText);
+            }
+            else
+            {
+                Debug.WriteLine("--> SOCKET_IO_MESSAGE:: {0} : {1}", e.Message.Event, e.Message.Json.ToJsonString());
+                //screenTbk.Text += e.Message.Json.ToJsonString();
+            }
+
+        }
+
+        private void connectBt_Click(object sender, RoutedEventArgs e)
+        {
+            socketIO = new Client(serverUrl.Text);
+            socketIO.Message += socketIO_Message;
+            socketIO.SocketConnectionClosed += socketIO_SocketConnectionClosed;
+            socketIO.Error += socketIO_Error;
+
+            socketIO.On("connect", (message) =>
+            {
+                Debug.WriteLine("on connect called!!!");
+                JObject jo = new JObject();
+                jo["publisher"] = "username";
+                jo["password"] = "password";
+                socketIO.Emit("hConnect", jo);
+            });
+            socketIO.ConnectAsync();
+        }
+
+        void socketIO_Error(object sender, ErrorEventArgs e)
+        {
+            WebErrorStatus status = WebSocketError.GetStatus(e.Exception.HResult);
+            Debug.WriteLine("-->SOCKET_IO_ERROR::" + status);
+        }
+
+        void socketIO_SocketConnectionClosed(object sender, EventArgs e)
+        {
+            Debug.WriteLine(">>>socketIO_SocketConnectionClosed::Closed!");
+        }
+
+        private void disConnectBt_Click(object sender, RoutedEventArgs e)
+        {
+            if (socketIO != null)
+            {
+                socketIO.Close();
+            }
+        }
+
+        private void sendBt_Click(object sender, RoutedEventArgs e)
+        {
+            if (socketIO != null && socketIO.IsConnected)
+            {
+                socketIO.Emit("hMessage", JObject.Parse(sendTbx.Text));
+            }
+        }
+
+    }
+}
